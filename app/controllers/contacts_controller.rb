f8002f7ab3d23abb6ff6b6ae8990a988a4a15804
Stefanie Holbrook <stefholbrook@gmail.com>
2016-01-05 04:00:18 +0000

    created contact form and fixed bug

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,9 @@
 class ContactsController < ApplicationController
-    def new
-    end
-    
-    def create
-    end
-    
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+  end
+  
 end
