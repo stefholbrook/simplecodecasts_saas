4813d7ff432f50dfb8fac9f1f7105f8beedfdae8
Stefanie Holbrook <stefholbrook@gmail.com>
2016-01-09 21:32:42 +0000

    Added profile model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160108233842) do
-=======
-ActiveRecord::Schema.define(version: 20160109185648) do
->>>>>>> stripe_integration
+ActiveRecord::Schema.define(version: 20160109212941) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -28,6 +24,18 @@ ActiveRecord::Schema.define(version: 20160109185648) do
   create_table "plans", force: true do |t|
     t.string   "name"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
